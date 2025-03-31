b3c8c6e7d54f59940426c06d64627f3434b6be0f
Halyna Moldovianu <Beorgeos@gmail.com>
2025-03-31 17:00:11 +0200

    1

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import userData from "./data/userData.json";
+import Profile from "./components/Profile";
+import friendsData from "./data/friendsData.json";
+import FriendList from "./components/FriendList";
+import TransactionHistory from "./components/TransactionHistory";
+import transactions from "./data/transactionsData.json";
+
+function App() {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friendsData={friendsData} />
+      <TransactionHistory items={transactions} />
+    </>
+  );
+}
+
+export default App;
